f0266fa11c9a093a2afadc84066ba07d181ded85
ThomasTritscher <thomas.tritscher33@googlemail.com>
2021-06-12 11:29:56 +0200

    added new questions

--- game.js
+++ game.js
@@ -1,58 +1,58 @@
 let allQuestions = [
   {
-    'question': 'Wer hat HTML erfunden?',
-    'answer_1': 'Donald Duck',
-    'answer_2': 'Bruce Willis',
-    'answer_3': 'Bill Gates',
-    'answer_4': 'Tim Berners-Lee',
+    'question': 'Wie lautet ALF`s vollständiger Name?',
+    'answer_1': 'Henry Thomas',
+    'answer_2': 'Wayne Schlegel',
+    'answer_3': 'Willie Tanner',
+    'answer_4': 'Gordon Shumway',
     'right_answer': 4
   },
   {
-    'question': 'Was bedeutet das HTML Tag &lta&gt?',// Tag muss als Text umgewandelt werden
-    'answer_1': 'Text Fett',
-    'answer_2': 'Container',
-    'answer_3': 'Ein Link',
-    'answer_4': 'Kursiv',
+    'question': 'Wie lautet der Name von Michael Knight `s Boss',
+    'answer_1': 'Tempelton Peck',
+    'answer_2': 'Dr. Bonnie Barstow',
+    'answer_3': 'Devon Miles',
+    'answer_4': 'David Hasselhoff',
     'right_answer': 3
   },
   {
-    'question': 'Wie bindet man eine Website in eine Website ein?',
-    'answer_1': '&lt;iframe&gt;, &lt;frame&gt; and &lt;frameset&gt;',
-    'answer_2': '&lt;iframe&gt;',
-    'answer_3': '&lt;frame&gt;',
-    'answer_4': '&lt;frameset&gt;',
+    'question': 'Wer hat den Titelsong zu Miami Vice geschrieben?',
+    'answer_1': 'Sonny Crockett',
+    'answer_2': 'Jan Hammer',
+    'answer_3': 'Ricardo Tubs',
+    'answer_4': 'Martin Costillo',
     'right_answer': 2
   },
   {
-    'question': 'Wie stellt man Text am BESTEN fett dar?',
-    'answer_1': '&lt;strong&gt;',
-    'answer_2': 'CSS nutzen',
-    'answer_3': '&lt;bold&gt;',
-    'answer_4': '&lt;b&gt;',
+    'question': 'In welchem Jahr erschien der Film TRON?',
+    'answer_1': '1982',
+    'answer_2': '1984',
+    'answer_3': '1986',
+    'answer_4': '1988',
     'right_answer': 1
   },
   {
-    'question': 'Welches Attribut kann man NICHT für Textarea verwenden?',
-    'answer_1': 'readonly',
-    'answer_2': 'max',
-    'answer_3': 'from',
-    'answer_4': 'spellcheck',
+    'question': 'Wann erschien Michael Jackson`s Album Thriller?',
+    'answer_1': '1982',
+    'answer_2': '1987',
+    'answer_3': '1989',
+    'answer_4': '1985',
     'right_answer': 1
   },
   {
-    'question': 'Wie wählst du alle Elemenete vom Typ &lt;a&gt; mit dem attribut titile aus?',
-    'answer_1': 'a[title]{...}',
-    'answer_2': 'a&gt;title{...}',
-    'answer_3': 'a.title{...}',
-    'answer_4': 'a=title{...}',
+    'question': 'Wie heißt das Monster von den Goonies?',
+    'answer_1': 'Sloth',
+    'answer_2': 'Data',
+    'answer_3': 'Clark Devereuax',
+    'answer_4': 'Chunk',
     'right_answer': 1
   },
   {
-    'question': 'Wie definiert man in Javascript eine Variable?',
-    'answer_1': 'let 100 = rate;',
-    'answer_2': '100 = let rate;',
-    'answer_3': 'rate = 100;',
-    'answer_4': 'let rate = 100;',
+    'question': 'Wievviel Bit hat ein Commodore 64?',
+    'answer_1': '6',
+    'answer_2': '4',
+    'answer_3': '64',
+    'answer_4': '8',
     'right_answer': 4
   }
 ];
@@ -102,6 +102,8 @@ function loadQuestion() {
   document.getElementById('answer3').innerHTML = allQuestions[question_number - 1]['answer_3'];
   document.getElementById('answer4').innerHTML = allQuestions[question_number - 1]['answer_4'];
   right_answer = allQuestions[question_number - 1]['right_answer'];
+  document.getElementById('progress-bar').innerHTML = progress + '%';// Änderung der Baranzeige um je 10% pro nextQuestion
+    document.getElementById('progress-bar').style.width = progress + '%'; //Breite des Styles/Bg wird um je 10% erhöht
 
 }
 
